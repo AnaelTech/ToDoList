75804e2f4d2e2e22cc102672cadfe187e74412ef
Anael Payet <anaelpayetpro@gmail.com>
2024-11-20 16:58:08 +0100

    update test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Ajouter le répertoire racine (où se trouve 'app') au sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
 from fastapi.testclient import TestClient
 from app.main import app
 
